ac33ddfe8989991be345227c06a929368f690a79
starmariner <70407810+StarMariner@users.noreply.github.com>
2020-11-03 19:51:47 +0000

    Update LAN_CHECK.bat

--- LAN_CHECK.bat
+++ LAN_CHECK.bat
@@ -6,6 +6,8 @@ REM ___ = online , _ _ =offline
 REM Ping -w value is in ms so it might be to quick?
 REM esc characters are for colour changes to text.
 REM NOTE: SmartPhones seem to loose connection during a sleep cycle
+REM I use NSlookup to find the default server. If I use IPconfig it outputs to many default gateways for Network adapters
+REM This only works for small networks That you know the range of . 
 
 REM the number after the DOT is Missed off on purpose, its put back later for the range 1-254
 SET ip=192.168.1.
@@ -19,7 +21,7 @@ ECHO ------------ LAN Check. Lookup and Ping ------------------
 ECHO      ___ = ON-LINE   _ _ = OFFLINE or Sleeping.
 ECHO      To quit looping Type Ctrl+C or Ctrl+break  
 ECHO/
-nslookup host 2>1
+nslookup * 2>nul
 ECHO [97m STATUS  IP address    NS Lookup Name
 
 SETLOCAL ENABLEDELAYEDEXPANSION
@@ -28,8 +30,8 @@ FOR /L %%R IN (%RangeStart%,1,%RangeEnd%) DO (
 			SET /A count=!count! +1
 			SET LKUP=%ip%!count!
 			SET line_status=[31m  _ _
-			FOR /f "tokens=2" %%i IN ('nslookup %%LKUP%% 2^>^&1 ^| findstr /C:"Name"') DO (
-			                                                                               FOR /f "tokens=4" %%j IN ('ping -n 1 -w 250 %%LKUP%% ^| findstr /C:"Reply"') DO SET line_status=[32m  ___
+			FOR /f "tokens=2" %%i IN ('nslookup %%LKUP%% 2^>Nul ^| findstr /C:"Name"') DO (
+			                                                                               FOR /f "tokens=4" %%j IN ('ping -n 1 -w 500 %%LKUP%% ^| findstr /C:"Reply"') DO SET line_status=[32m  ___
 																						   SET nicename=%%i
 																						   IF NOT !nicename!==UNKNOWN ECHO !Line_status!  [0m  !LKUP!   !nicename!  
 																						  )
